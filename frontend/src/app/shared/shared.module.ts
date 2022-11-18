5c2d2453f9949a9762cf67dc1333292188f77edd
ti-ben <benoit.titeux@protonmail.com>
2022-11-18 16:20:25 +0100

    Initial commit

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,8 +1,17 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadcrumbComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ],
